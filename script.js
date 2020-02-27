59bd206da6bff87896e50a51a44c66aadaa19435
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-02-27 14:19:54 -0500

    added background randomizer

--- script.js
+++ script.js
@@ -30,6 +30,16 @@ const getRandomNum = (num) => {
 const moveDuck = () => {
     const w = window.innerWidth;
     const h = innerHeight;
-    duck.style.top = getRandomNum(w)/1.5 + "px";
-    duck.style.left = getRandomNum(h)/1.5 + "px";
+    duck.style.top = getRandomNum(w) * .5  + "px";
+    duck.style.left = getRandomNum(h) + "px";
 };
+
+
+
+const totalCount = 5;
+function ChangeIt() 
+{
+let num = Math.ceil( Math.random() * totalCount );
+document.body.background = 'images/'+num+'.png';
+document.body.style.backgroundSize = "cover";// Background repeat
+}
