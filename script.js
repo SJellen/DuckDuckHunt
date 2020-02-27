9cedba8bc98093612a0f915a48d66e78bbf38b45
scott jellen <scottjellen@scotts-MBP.fios-router.home>
2020-02-27 16:01:02 -0500

    edit

--- script.js
+++ script.js
@@ -13,11 +13,8 @@ duck.addEventListener("click", () => {
 const increaseScore = () => {
 
     const score = document.querySelector("#score-counter").innerHTML;
-
     const scoreHTML = document.querySelector("#score-counter");
-
     let count = Number(score);
-
     scoreHTML.innerHTML = count + 1;
 };
 
